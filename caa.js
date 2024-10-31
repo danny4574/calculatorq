fc4a0101c2667b0bbc31d5fbbf652649ea859408
sinss <okeloadaniel07@gmail.com>
2024-10-31 15:02:59 +0100

    claculator

---
+++ caa.js
@@ -0,0 +1,18 @@
+function displayresult(){
+    let output = document.querySelector(".output");
+
+    output.value = eval(output.value)
+}
+
+function addoutput(val){
+    let output = document.querySelector(".output")
+    output.value =output.value + val
+    
+}
+function removeoutput(){
+    let output = document.querySelector(".output")
+    output.value = " "
+}
+function changecolor(){
+    
+}
